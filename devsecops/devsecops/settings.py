ccd801d0c9b2064d9baa4788a6bbe4e12ca6653b
Oscar Castillo <del.castillo.oscar.a@gmail.com>
2021-06-07 19:45:36 +0200

    test

--- devsecops/devsecops/settings.py
+++ devsecops/devsecops/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+c=np!en3#2mczi&n4v&@zu_zb*)dke9ya($oqrftj!j+ihgf8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
